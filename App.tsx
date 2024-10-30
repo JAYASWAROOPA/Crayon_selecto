f6cfc1917b864d12a7f63f1476b2b7354d64a15b
JAYASWAROOPA <jayaswaroopa.ec23@bitsathy.ac.in>
2024-10-30 11:52:48 +0530

    included provider

--- App.tsx
+++ App.tsx
@@ -1,22 +1,28 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
+import { Provider } from "react-redux";
+import store from "./store.js"; 
 import Metanas_ui_design from "./src/screens/Metanas_ui_design/MetanasUiDesign.jsx";
 import WorkSpace from "./src/screens/WorkSpace/WorkSpace.jsx";
 import CreateExpense from "./src/components/Createexpense/createExpense.jsx";
 import Doccument from "./src/screens/DocumentScreen/doccument.jsx";
 import FolderPage from "./src/screens/Folder/folderPage.jsx";
+
 const Stack = createStackNavigator();
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Metanas" component={Metanas_ui_design} />
-        <Stack.Screen name="WorkSpace" component={WorkSpace} />
-        <Stack.Screen name="CreateExpense" component={CreateExpense} />
-        <Stack.Screen name="Doccument" component={Doccument} />
-        <Stack.Screen name="Folder" component={FolderPage} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Metanas" component={Metanas_ui_design} />
+          <Stack.Screen name="WorkSpace" component={WorkSpace} />
+          <Stack.Screen name="CreateExpense" component={CreateExpense} />
+          <Stack.Screen name="Doccument" component={Doccument} />
+          <Stack.Screen name="Folder" component={FolderPage} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
